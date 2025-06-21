4656b96195a3252b22d9fe611166880d457a5f80
Parina89 <pvag7076@gmail.com>
2025-06-21 12:39:24 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -43,7 +43,7 @@ def load_model():
     #url = 'https://drive.google.com/file/d/1rWsqzW6UIL5pxjczNH72d3_3WUl27Rza/view?usp=drive_link'
     #urllib.request.urlretrieve(url, 'textile.h5')
     model_path = "textile.pth"  # <-- Update path
-    model.load_state_dict(torch.load("textile.pth", map_location=torch.device("cpu")))
+    model = torch.load(model_path, map_location=device)
     model.eval()
     return model
 
