4080891654ed5ce97476f921ef3cd7a2384bf751
Parina89 <pvag7076@gmail.com>
2025-06-21 11:50:43 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -42,7 +42,7 @@ def load_model():
     model = FabricDefectClassifier()
     #url = 'https://drive.google.com/file/d/1rWsqzW6UIL5pxjczNH72d3_3WUl27Rza/view?usp=drive_link'
     #urllib.request.urlretrieve(url, 'textile.h5')
-    model_path = "textile.pth"  # <-- Update path
+    model_path = "textile (1).pth"  # <-- Update path
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.eval()
     return model.to(device)
