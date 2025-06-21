0d538d0cbee6b8ca5a1d56d9cc776d01a66b142a
Parina89 <pvag7076@gmail.com>
2025-06-21 12:47:02 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -6,6 +6,7 @@ from PIL import Image
 import numpy as np
 import os
 import urllib.request
+import torch.nn.functional as F
 
 # Set page config
 st.set_page_config(page_title="Fabric Inspector", page_icon="🧵")
@@ -27,18 +28,26 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Define model architecture (Simple Transfer Learning)
 class FabricDefectClassifier(nn.Module):
-    def __init__(self, num_classes=2):
+    def __init__(self):
         super(FabricDefectClassifier, self).__init__()
-        self.model = models.resnet18(pretrained=True)  # Using ResNet18
-        num_ftrs = self.model.fc.in_features
-        self.model.fc = nn.Linear(num_ftrs, num_classes)  # 2 classes: Defect-Free, Stain
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(32 * 56 * 56, 128)
+        self.fc2 = nn.Linear(128, 2)
 
     def forward(self, x):
-        return self.model(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 32 * 56 * 56)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
 # Load model (Assuming you have trained model stored as 'fabric_model.pth')
 @st.cache(allow_output_mutation=True)
 def load_model():
+    model = FabricDefectClassifier()
     #url = 'https://drive.google.com/file/d/1rWsqzW6UIL5pxjczNH72d3_3WUl27Rza/view?usp=drive_link'
     #urllib.request.urlretrieve(url, 'textile.h5')
     model_path = "textile.pth"  # <-- Update path
