1ad8255979284880f184cb11968f7651863d53a5
Parina89 <pvag7076@gmail.com>
2025-06-24 12:54:00 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -67,7 +67,7 @@ def get_prediction(image):
     outputs = model(image)          # Get model output (logits or probabilities)
     #_, predicted = torch.max(outputs, 1)  # Pick the class with highest score
     probs = torch.sigmoid(outputs).detach().cpu().numpy().squeeze()
-    label = "defect-free" if probs <= 0.5 else "stain"
+    label = "defect-free" if probs.item() <= 0.5 else "stain"
     return label
 
 class_labels = ['defect-free','stain']# adjust as per your training labels
