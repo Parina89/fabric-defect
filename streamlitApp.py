0ec2221346daf0420718780e7be96f4c6247b8a3
Parina89 <pvag7076@gmail.com>
2025-06-25 22:30:56 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -69,10 +69,11 @@ def get_prediction(image):
     probs = torch.sigmoid(outputs).detach().cpu().numpy().squeeze()
     print("Model probabilities:" , probs)  # Temporarily add this for debugging
     #label = "defect-free" if probs[0] <= 0.5 else "stain"
-    if len(probs.shape) == 0:  # Single output (binary classification)
-       label = "stain" if probs > 0.5 else "defect-free"
-    else: # Multiple outputs
-       label = "stain" if probs[0] > 0.5 else "defect-free"
+    label = "stain" if probs[0] > threshold else "defect-free"
+    #if len(probs.shape) == 0:  # Single output (binary classification)
+       #label = "stain" if probs > 0.5 else "defect-free"
+    #else: # Multiple outputs
+       #label = "stain" if probs[0] > 0.5 else "defect-free"
     return label
 
 class_labels = ['defect-free','stain'] # adjust as per your training labels
