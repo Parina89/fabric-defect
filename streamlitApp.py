4c077cb5a5eb2e3ad8259c2cda8758c1e4e59df0
Parina89 <pvag7076@gmail.com>
2025-07-01 13:31:29 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -66,7 +66,7 @@ def get_prediction(image):
     image = transform_image(image)  # Add batch dimension
     outputs = model(image)          # Get model output (logits or probabilities)
     _, predicted = torch.max(outputs, 1)  # Pick the class with highest score
-    #probs = torch.sigmoid(outputs).detach().cpu().numpy().squeeze()
+    probs = torch.sigmoid(outputs).detach().cpu().numpy().squeeze()
     #print("Model probabilities:" , probs)  # Temporarily add this for debugging
     #label = "defect-free" if probs[0] <= 0.5 else "stain"
     if len(probs.shape) == 0:  # Single output (binary classification)
