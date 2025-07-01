cd81d79834d095162ec788dad9f107ff75bc7613
Parina89 <pvag7076@gmail.com>
2025-07-01 13:04:10 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -70,7 +70,7 @@ def get_prediction(image):
     #print("Model probabilities:" , probs)  # Temporarily add this for debugging
     #label = "defect-free" if probs[0] <= 0.5 else "stain"
     if len(probs.shape) == 0:  # Single output (binary classification)
-        label = "defect-free" if probs > 0.5 else "stain"
+        label = "defect-free" if probs > 0.5 else "defect-free"
     else:  # Multiple outputs
         label = "stain" if probs[0] > 0.5 else "stain"
     return label
