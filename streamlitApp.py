0194bb75dd4eae49b3d372b15745ec511cf3b2c3
Parina89 <pvag7076@gmail.com>
2025-07-02 10:33:30 +0530

    Update streamlitApp.py

--- streamlitApp.py
+++ streamlitApp.py
@@ -13,7 +13,7 @@ st.write("Upload an image of fabric and the AI model will classify it as 'Defect
 st.markdown("""
 <style>
 .stApp {
-    background-color: #B0E0E6;
+    background-color: #2F4F4F;
 }
 </style>
 """, unsafe_allow_html=True)
